c8469b4b38f6d4a37090d422214f5c5ffb8dfefa
Lam <lttlam1710@gmail.com>
2023-11-11 22:24:10 +0700

    DoubleJump

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     [SerializeField] private float moveSpeed = 1f;
     private float jumpForce = 8f;
     private Rigidbody2D rb;
+    private bool canDoubleJump;
 
     //Check Ground
     [SerializeField] public bool isGrounded; // Check Ground
@@ -40,20 +41,35 @@ public class Player : MonoBehaviour
             distance,           // Do dai
             groundlayer);       // Layer 
 
-        rb.velocity = new Vector2 (moveSpeed, rb.velocity.y); // Auto MoveRight * Speed
-
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded ) // Neu bam nut Jump + ground check ok
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        rb.velocity = new Vector2(moveSpeed, rb.velocity.y); // Auto MoveRight * Speed
+        if (Input.GetKeyDown(KeyCode.Space))  
+            Jump();
 
         //Anim State
-        
+
         anim.SetFloat("xInput", rb.velocity.x);       // Running State
         anim.SetBool("isGrounded", isGrounded);     // Jump Check Ground
-        
-        
+        anim.SetBool("canDoubleJump", canDoubleJump);
+
         if (rb.velocity.x != 0f)                     // Check velocity anim State machine (Jump)
-            anim.SetFloat("yInput", rb.velocity.y);    
+            anim.SetFloat("yInput", rb.velocity.y);
     }
+
+    private void Jump()
+    {
+        if (isGrounded) // Neu bam nut Jump + ground check ok
+        {
+            canDoubleJump = true;
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+        else if (canDoubleJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            canDoubleJump = false;
+
+        }
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.DrawLine(transform.position, new Vector2 (transform.position.x, transform.position.y - distance));
